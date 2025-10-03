1179a332b695e9e2c1c8c7ffac702ce08b026dbe
elgabrielcisneros <tilordqwertydev3@gmail.com>
2025-10-02 22:30:09 -0400

    remove unused tab

--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -17,11 +17,6 @@ export const routes: Routes = [
           import('src/app/pages/tab2/tab2.page').then((m) => m.Tab2Page),
       },
       {
-        path: 'tab3',
-        loadComponent: () =>
-          import('src/app/pages/tab3/tab3.page').then((m) => m.Tab3Page),
-      },
-      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full',
