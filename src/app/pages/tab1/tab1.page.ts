ed0275f95b6bda8d2fe525e1b4694a3e4485fb98
elgabrielcisneros <tilordqwertydev3@gmail.com>
2025-10-02 22:29:42 -0400

    scanning in progress

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from 'src/app/explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import {
+  IonContent,
+  IonLabel,
+  IonButton,
+  IonGrid,
+  IonCol,
+  IonRow
+} from '@ionic/angular/standalone';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonContent,
+    IonLabel,
+    IonButton,
+    IonGrid,
+    IonCol,
+    IonRow
+  ],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit {
+  isSupported = false;
+  barcodes: Barcode[] = [];
+
+  constructor( private alertController: AlertController ) {}
+
+  ngOnInit(): void {
+    BarcodeScanner.isSupported().then((result) => {
+          this.isSupported = result.supported;
+        });  }
+
+  scanQRCode(){
+    // Implement QR code scanning logic here
+  }
+
+  async scanQrCode(): Promise<void> {
+      const granted = await this.requestPermissions();
+      if (!granted) {
+        this.presentAlert();
+        return;
+      }
+      const { barcodes } = await BarcodeScanner.scan();
+      this.barcodes.push(...barcodes);
+    }
+
+    async requestPermissions(): Promise<boolean> {
+      const { camera } = await BarcodeScanner.requestPermissions();
+      return camera === 'granted' || camera === 'limited';
+    }
+
+    async presentAlert(): Promise<void> {
+      const alert = await this.alertController.create({
+        header: 'Permission denied',
+        message: 'Please grant camera permission to use the barcode scanner.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    }
 }
