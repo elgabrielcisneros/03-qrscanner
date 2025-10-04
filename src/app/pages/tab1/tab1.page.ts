2ae862d2b0a36253007a0b062212db8ad5573a5d
elgabrielcisneros <tilordqwertydev3@gmail.com>
2025-10-04 19:59:08 -0400

    add try catch
    - test in mobile in progress

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  IonContent,
-  IonLabel,
-  IonButton,
-  IonGrid,
-  IonCol,
-  IonRow
-} from '@ionic/angular/standalone';
+import { IonContent, IonLabel, IonButton } from '@ionic/angular/standalone';
 import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 import { AlertController } from '@ionic/angular';
 
@@ -14,31 +7,22 @@ import { AlertController } from '@ionic/angular';
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [
-    IonContent,
-    IonLabel,
-    IonButton,
-    IonGrid,
-    IonCol,
-    IonRow
-  ],
+  imports: [IonContent, IonLabel, IonButton],
 })
 export class Tab1Page implements OnInit {
   isSupported = false;
   barcodes: Barcode[] = [];
 
-  constructor( private alertController: AlertController ) {}
+  constructor(private alertController: AlertController) {}
 
   ngOnInit(): void {
     BarcodeScanner.isSupported().then((result) => {
-          this.isSupported = result.supported;
-        });  }
-
-  scanQRCode(){
-    // Implement QR code scanning logic here
+      this.isSupported = result.supported;
+    });
   }
 
   async scanQrCode(): Promise<void> {
+    try {
       const granted = await this.requestPermissions();
       if (!granted) {
         this.presentAlert();
@@ -46,19 +30,23 @@ export class Tab1Page implements OnInit {
       }
       const { barcodes } = await BarcodeScanner.scan();
       this.barcodes.push(...barcodes);
+      console.log('Scanned QR code:', barcodes);
+    } catch (error) {
+      console.error('Error scanning QR code:', error);
     }
+  }
 
-    async requestPermissions(): Promise<boolean> {
-      const { camera } = await BarcodeScanner.requestPermissions();
-      return camera === 'granted' || camera === 'limited';
-    }
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
 
-    async presentAlert(): Promise<void> {
-      const alert = await this.alertController.create({
-        header: 'Permission denied',
-        message: 'Please grant camera permission to use the barcode scanner.',
-        buttons: ['OK'],
-      });
-      await alert.present();
-    }
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
