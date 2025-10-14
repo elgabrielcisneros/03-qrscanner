3de2da90b31649f5a021c202d2f4773b63925748
elgabrielcisneros <tilordqwertydev3@gmail.com>
2025-10-13 21:57:17 -0400

    create model for scanning types

---
+++ src/app/models/qr-log.model.ts
@@ -0,0 +1,47 @@
+export class QrLog {
+  public bytes!: number[];
+  public displayValue!: string;
+  public format!: string;
+  public rawValue!: string;
+  public valueType!: string;
+  public icon!: string;
+  public created!: Date;
+  public urlBookmark?: {
+    title: string;
+    url: string;
+  };
+
+  constructor(format: string, rawValue: string) {
+    this.format = format;
+    this.rawValue = rawValue;
+
+    this.created = new Date();
+    this.defineType();
+  }
+
+  private defineType() {
+    const guessType = this.rawValue.substring(0, 4);
+    console.info('guessType', guessType);
+
+    switch (guessType) {
+      case 'http':
+        this.valueType = 'url';
+        this.icon = 'globe';
+        break;
+
+      case 'tel:':
+        this.valueType = 'tel';
+        this.icon = 'call';
+        break;
+
+      case 'geo:':
+        this.valueType = 'geo';
+        this.icon = 'pin';
+        break;
+
+      default:
+        this.valueType = 'not found';
+        this.icon = 'create';
+    }
+  }
+}
