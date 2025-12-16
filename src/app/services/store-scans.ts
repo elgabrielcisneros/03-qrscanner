98ce5aad4062fd4ad8984acc5e23cee75046d508
elgabrielcisneros <tilordqwertydev3@gmail.com>
2025-12-16 12:35:44 -0400

    store scans at indexedDB✅
    - use ionic storage

--- src/app/services/store-scans.ts
+++ src/app/services/store-scans.ts
@@ -1,22 +1,40 @@
-import { Injectable } from "@angular/core";
-import { QrLog } from "../models/qr-log.model";
-
+import { Injectable } from '@angular/core';
+import { QrLog } from '../models/qr-log.model';
+import { Storage } from '@ionic/storage-angular';
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class StoreScans {
   // this is a public property
   storedScans: QrLog[] = [];
+  // this is a private property
+  private _storage: Storage | null = null;
 
-  constructor(){
-    
+  constructor(private storage: Storage) {
+    this.init();
+    this.getStoredScans();
   }
 
-  storeScanLog(format: string, rawValue: string) {
+  async init() {
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+
+  async getStoredScans() {
+    const storedScans = await this.storage.get('scans');
+    this.storedScans = storedScans || [];
+  }
+
+  async storeScanLog(format: string, rawValue: string) {
     const newLog = new QrLog(format, rawValue);
 
     // moving new scans at the array beginning
     this.storedScans.unshift(newLog);
-    console.info("stored", this.storedScans);
+    console.info('stored', this.storedScans);
+    await this.storage.set('scans', this.storedScans);
   }
 }
